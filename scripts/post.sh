2f9d3231c32cbfabd9313883e085655053d4da13
Josh Gavant <joshgavant@gmail.com>
2019-02-25 14:49:55 -0800

    explicitly specify py3

--- scripts/post.sh
+++ scripts/post.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 
-python manage.py migrate
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', 'Password1')" | python manage.py shell
+/opt/python/3.7/bin/python3 manage.py migrate
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', 'Password1')" | /opt/python/3.7/bin/python3 manage.py shell
