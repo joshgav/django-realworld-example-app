664d3aa66f7f4c6ed6fab6c3e29e250b74a50ebb
Josh Gavant <joshgavant@gmail.com>
2019-03-19 10:47:25 -0700

    touch db file before using

--- scripts/post.sh
+++ scripts/post.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/env bash
 
 python=/opt/python/3.7/bin/python3
+persist=/var/azure/sqlite
 
+touch ${persist}/db.sqlite3
 $python manage.py migrate
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', 'Password1')" | $python manage.py shell
